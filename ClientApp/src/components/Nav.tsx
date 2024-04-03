a5b47d90b14fe65fb0403c141bb8dc3c0c8d4ed8
Kristy <kprieto14@gmail.com>
2024-04-02 21:33:25 -0400

    Change font style

--- ClientApp/src/components/Nav.tsx
+++ ClientApp/src/components/Nav.tsx
@@ -8,8 +8,8 @@ export function Nav() {
     return (
     <nav>
         <Container>
-                <Row className="d-flex align-items-center ">
-                    <Col sm={4} className="d-flex justify-content-start">
+                <Row>
+                    <Col sm={4} className="d-flex justify-content-start align-items-center">
                         <Link to={'/'}>
                             <div style={{display: "flex", justifyContent: "center", gap:"5px"}}>
                                 <GiBirdHouse size={30} title="Bird cartoon facing to the right"/>
@@ -18,12 +18,13 @@ export function Nav() {
                         </Link>
                     </Col>
                     
-                    <Col sm={4} className="d-flex justify-content-center">
+                    <Col sm={4} className="d-flex justify-content-center align-items-center">
                             <img src={ birdLogo } alt="Logo of a colorful bird" />
                     </Col>
                     
-                    <Col sm={4} className="d-flex justify-content-end">
-                        <Link to={'/'}>Login</Link>
+                    <Col sm={4} className="d-flex justify-content-end align-items-center">
+                        {/* Span is needed for hover to work correctly for some reason */}
+                        <Link to={'/'}><span>Login</span></Link>
                     </Col>
                 </Row>
         </Container>
