9b798513ce256e2a2d0fda785dd2a972536da37f
Kristy <kprieto14@gmail.com>
2024-04-27 00:36:20 -0400

    Update add bird page to current theme

--- ClientApp/src/pages/AddBird.tsx
+++ ClientApp/src/pages/AddBird.tsx
@@ -1,67 +1,87 @@
 import React from "react";
-import { Button, Card, Col, Form, Row } from "react-bootstrap";
-import { MdAddAPhoto } from "react-icons/md";
+import { Col, Form, Row } from "react-bootstrap";
+import { GiNestBirds } from "react-icons/gi";
 
 export function AddBird() {
+    const yearsOfRelease = ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
+
     return (
         <section className="add-bird">
-            <h3>Add a Bird to Your Aviary</h3>
-            
-            <Card className="add-bird-card">
+            <div className="middle-card w-100">
+                <GiNestBirds className="react-bird-icon mb-3"/>
+                <h2 className="mb-4">Add a Bird to Your Aviary</h2>
                 <Form>
-                    <Form.Group controlId="formFile" className="mb-3">
-                        <Form.Label className="h3"><span style={{display: "flex", alignItems: "center", gap:"5px"}}><MdAddAPhoto /> Upload Bird Photo</span></Form.Label>
-                        <Form.Control type="file" />
-                    </Form.Group>
-
                     <Row>
                         <Col>
-                            <Form.Group className="" controlId="">
-                                <Form.Label className="h3">Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter bird name"></Form.Control>
-                            </Form.Group>
+                            <Form.Label className="h4 mb-3">Name</Form.Label>
+                            <Form.Control
+                                type="text" 
+                                placeholder="Enter name of bird" 
+                                className="mb-3 input"
+                                size="lg"
+                                onChange={() => console.log("Changed")}
+                            />
                         </Col>
 
                         <Col>
-                            <Form.Group className="" controlId="">
-                                <Form.Label className="h3">Adopted From</Form.Label>
-                                <Form.Control type="text" placeholder="Enter store name"></Form.Control>
-                            </Form.Group>
+                            <Form.Label className="h4 mb-3">Adopted From</Form.Label>
+                            <Form.Control
+                                type="text" 
+                                placeholder="Enter where you bought your bird" 
+                                className="mb-3 input"
+                                size="lg"
+                                onChange={() => console.log("Changed")}
+                            />
                         </Col>
                     </Row>
-                    
 
                     <Row>
                         <Col>
-                            <Form.Group className="" controlId="">
-                                <Form.Label className="h3">Year</Form.Label>
-                                <Form.Control type="text" placeholder="Enter year of release"></Form.Control>
-                            </Form.Group>
+                            <Form.Label className="h4 mb-3">Year</Form.Label>
+                            <Form.Select
+                                defaultValue="2012" 
+                                className="mb-3 input"
+                                size="lg"
+                                onChange={() => console.log("Changed")}
+                            >
+                                {yearsOfRelease.map((year, index) => (
+                                    <option key={index} value={year}>{year}</option>
+                                ))}
+                            </Form.Select>
                         </Col>
 
                         <Col>
-                            <Form.Group className="" controlId="">
-                                <Form.Label className="h3">Season</Form.Label>
-                                <Form.Select aria-label="Default select example">
-                                    <option value={'Spring'}>Spring</option>
-                                    <option value={'Summer'}>Summer</option>
-                                    <option value={'Fall'}>Fall</option>
-                                    <option value={'Winter'}>Winter</option>
-                                </Form.Select>
-                            </Form.Group>
+                            <Form.Label className="h4 mb-3">Season</Form.Label>
+                            <Form.Select
+                                defaultValue="Spring" 
+                                className="mb-3 input"
+                                size="lg"
+                                onChange={() => console.log("Changed")}
+                            >
+                                <option>Spring</option>
+                                <option>Summer</option>
+                                <option>Fall</option>
+                                <option>Winter</option>
+                            </Form.Select>
                         </Col>
                     </Row>
 
                     <Form.Group className="" controlId="">
-                        <Form.Label className="h3">Holiday</Form.Label>
-                        <Form.Control type="text" placeholder="Enter holiday of release"></Form.Control>
+                        <Form.Label className="h4 mb-3">Holiday</Form.Label>
+                        <Form.Control
+                                type="text" 
+                                placeholder="Enter the holiday of release" 
+                                className="mb-3 input"
+                                size="lg"
+                                onChange={() => console.log("Changed")}
+                        />
                     </Form.Group>
 
-                    <Button variant="primary" type="submit">
-                        + Add Bird
-                    </Button>
+                    <button className="gradient-button w-100 mt-3" onClick={() => console.log("Clicked")}>
+                        <h5>Add Bird</h5>
+                    </button>
                 </Form>
-            </Card>
+            </div>
         </section>
     )
 }
