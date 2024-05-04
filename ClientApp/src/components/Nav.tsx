f0b6d9af868630e889cd8b5e23d0e81e46c8227d
Kristy <kprieto14@gmail.com>
2024-05-03 20:57:57 -0400

    Update navbar to change based on login status

--- ClientApp/src/components/Nav.tsx
+++ ClientApp/src/components/Nav.tsx
@@ -2,42 +2,65 @@ import React from "react";
 import { GiBirdHouse } from "react-icons/gi";
 import IconCenter from "./IconCenter";
 import { Link } from "react-router-dom";
+import { isLoggedIn } from "../api/auth";
 
 export function Nav() {
+    const loginStatus = isLoggedIn()
+
     return (
-    <header className="nav-header">
-        <nav>
-            <div>
-                <Link to={'/'}>
-                    <button className="blue-outline">
-                        <IconCenter reactIcon={ <GiBirdHouse/> } text="Home"/>
-                    </button>
-                </Link>
+        <header className="nav-header">
+            <nav>
+                <div>
+                    {/* Should only show if user is logged in */}
+                    {
+                        loginStatus ? (
+                            <>
+                                <Link to={'/'}>
+                                    <button className="blue-outline">
+                                        <IconCenter reactIcon={ <GiBirdHouse/> } text="Home"/>
+                                    </button>
+                                </Link>   
 
+                                    <Link to='/birdcage-list'>
+                                    <button className="blue-outline">
+                                        My Aviary
+                                    </button>
+                                </Link>                     
+                            </>
+                        ) 
+                        : null
+                    }
+                </div>
 
-                <Link to='/birdcage-list'>
-                    <button className="blue-outline">
-                        My Aviary
-                    </button>
-                </Link>
-             </div>
+                <div>
+                    {/* If user is logged in, make button to logout show, else show sign-in and register buttons */}
+                    {
+                        loginStatus ? (
+                            <>
+                                <Link to='/birdcage-list'>
+                                    <button className="blue-outline">
+                                        Logout
+                                    </button>
+                                </Link>                     
+                            </>
+                        ) 
+                        : 
+                        <>
+                            <Link to='/login'>
+                                <button className="blue-outline">
+                                    Log In
+                                </button>
+                            </Link>
 
-             <div>
-                <Link to='/login'>
-                    <button className="blue-outline">
-                        Log In
-                    </button>
-                </Link>
-                {/* Make this Change depending on if user is signed in or not */}
-
-
-                <Link to='/register'>
-                    <button className="gradient-button">
-                        Register
-                    </button>  
-                </Link>
-            </div>
-        </nav>
-    </header>
+                            <Link to='/register'>
+                                <button className="gradient-button">
+                                    Register
+                                </button>  
+                            </Link>
+                        </>
+                    }
+                </div>
+            </nav>
+        </header>
     )
 }
