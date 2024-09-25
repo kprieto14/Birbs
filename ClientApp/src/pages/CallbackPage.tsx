626beeaa6083a80411bc711b509a47f526ebd74d
Kristy <kprieto14@gmail.com>
2024-09-25 18:37:22 -0400

    Add a callback page for auth0 to redirect to on login

---
+++ ClientApp/src/pages/CallbackPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaUser } from "react-icons/fa6";
+
+export function CallbackPage() {
+    return (
+        <section className="sign-in">
+            <div className="middle-card w-100">
+                <FaUser className="react-icon mb-3"/>
+                <h2 className="mb-4">Welcome!</h2>
+            </div>
+        </section>  
+    )
+}
