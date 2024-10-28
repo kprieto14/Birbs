e77db7e0f06affd722d10eb6d480daa7f03ff0d7
Kristy <kprieto14@gmail.com>
2024-10-28 14:56:49 -0400

    Add ability to actually use Auth0 login component in application

--- ClientApp/src/auth0ProviderWithNavigate.tsx
+++ ClientApp/src/auth0ProviderWithNavigate.tsx
@@ -3,15 +3,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { useNavigate } from "react-router-dom";
 
 export const Auth0ProviderWithNavigate = ({ children }: PropsWithChildren<{}>) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL
 
   const onRedirectCallback = (appState: any) => {
     navigate(appState?.returnTo || window.location.pathname);
-  };
+  }
 
   if (!(domain && clientId && redirectUri)) {
     return null;
@@ -20,9 +20,9 @@ export const Auth0ProviderWithNavigate = ({ children }: PropsWithChildren<{}>) =
   return (
     <Auth0Provider
       domain={ domain }
-      clientId={ clientId }
-      authorizationParams={{
-        redirect_uri: redirectUri,
+        clientId={ clientId }
+        authorizationParams={{
+          redirect_uri: redirectUri,
       }}
       onRedirectCallback={ onRedirectCallback }
     >
--- ClientApp/src/components/Nav.tsx
+++ ClientApp/src/components/Nav.tsx
@@ -14,7 +14,7 @@ export function Nav() {
             returnTo: "/profile",
           },
         });
-      };
+    };
 
     function handleLogout() {
         logout()
